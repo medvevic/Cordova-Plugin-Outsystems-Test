8e380c61c5cdffdbc466f38b6e02ed77bfcae9f5
VICTOR MEDVEDEV <medvedev_victor@mail.ru>
2017-05-29 17:20:20 +0300

    29-05-2017 17:20

--- src/org/apache/cordova/passportscanner/PassportScannerPlugin.java
+++ src/org/apache/cordova/passportscanner/PassportScannerPlugin.java
@@ -43,10 +43,13 @@ import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
 */
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import android.util.Log;
@@ -77,10 +80,6 @@ public class PassportScannerPlugin extends CordovaPlugin {
 
     // I/O manager to handle new incoming serial data
     private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
-    //private SerialInputOutputManager mSerialIoManager;
-
-
-    //private UsbSerialDevice
     private UsbDevice device;
     private UsbDeviceConnection connection;
 
@@ -90,11 +89,7 @@ public class PassportScannerPlugin extends CordovaPlugin {
     private CordovaPlugin activityResultCallback = null;
     private ImageView imageView;
     private View errorView;
-    //private NotificationFlag stopReadingPassportFlag;
-    //private DeviceWrapper barcodeReaderDevice;
     private PassportScanner passportScanner;
-    //private EscPosPrinter escPosPrinter;
-    //private RegoPrinter contextRegoPrinter; // [VM]
     private TscPrinter tscPrinter;
     private String lastUrl;
 
@@ -1110,6 +1105,259 @@ public static class DeviceFinder {
     }
 
 }
+
+//--------------------------------------------------------------------------------------------------
+public class PassportScanner extends UsbSerialDevice {
+
+
+    public PassportScanner(UsbDevice device, UsbDeviceConnection connection) {
+        super(device, connection);
+    }
+
+    public String getVersion() throws IOException
+    {
+        if (mScannerVersion == null || mScannerVersion.isEmpty())
+        {
+            //Get Version
+            send((byte)'V', null, null);
+        }
+
+        return mScannerVersion;
+    }
+
+    public synchronized String[] waitMRZ(NotificationFlag stopFlag) throws IOException
+    {
+        mMRZ=null;
+
+        // Get MRZ
+
+        // Force reset of document detection, to detect document already in the slot
+        send((byte) 'C', new byte[]{0x00}, stopFlag);
+
+        send((byte) 'C', new byte[]{0x01}, stopFlag);
+
+        return mMRZ;
+    }
+
+    private static final String TAG = "PassportScanner";
+
+    private String mScannerVersion;
+
+    private String[] mMRZ = null;
+
+    private class ScannerPacket {
+
+        private byte mCode;
+        private int mLength;
+
+        private byte [] mData;
+        private int mDataPos = 0;
+
+        public ScannerPacket(byte code, int length) {
+            mCode = code;
+            mLength = length;
+
+            mData = new byte[mLength];
+            mDataPos = 0;
+        }
+
+        public ScannerPacket(byte code, byte [] data) {
+            mCode = code;
+            mLength = data.length;
+
+            mData = data;
+            mDataPos = mLength;
+        }
+
+        public int addData(byte [] data, int offset, int length) {
+            int toCopy = Math.min(mLength-mDataPos, length);
+
+            System.arraycopy(data, offset, mData, mDataPos, toCopy);
+            mDataPos += toCopy;
+            return toCopy;
+        }
+
+        public boolean isComplete() {
+            return mDataPos == mLength;
+        }
+
+        public byte getCode() {
+            return mCode;
+        }
+
+        public byte[] getData() {
+            return mData;
+        }
+    }
+
+    private BlockingQueue<PassportScanner.ScannerPacket> bq = (BlockingQueue<PassportScanner.ScannerPacket>) new ArrayBlockingQueue<PassportScanner.ScannerPacket>(16);
+    PassportScanner.ScannerPacket currentPacket = null;
+
+    byte[] tmpBuffer = new byte[256];
+    int tmpPos=0;
+
+    private void appendBuffer(byte [] data, int offset, int length) throws IOException {
+
+        if(256-tmpPos<length)
+            throw new IOException("Buffer size is too small.");
+
+        System.arraycopy(data, offset, tmpBuffer, tmpPos, length);
+        tmpPos += length;
+    }
+
+    private void resetResponse() {
+        currentPacket = null;
+    }
+
+    private PassportScanner.ScannerPacket unpackResponse(byte[] data) throws IOException {
+        int tmpLen = 0;
+        int usedLen = 0;
+
+        if(data==null)
+        {
+            if(tmpPos==0) // Padd with existing buffer
+            {
+                // Nothing to do
+                return null;
+            } else {
+                data = tmpBuffer;
+                tmpLen = tmpPos;
+            }
+        }
+        else if(tmpPos>0) // Padd with existing buffer
+        {
+            appendBuffer(data, 0, data.length);
+            data = tmpBuffer;
+            tmpLen = tmpPos;
+        }
+        else
+        {
+            tmpLen = data.length;
+        }
+
+        if(currentPacket==null) {
+            if (tmpLen<3)	{
+                //throw new IOException("Incomplete header response.");
+                return null;
+            }
+
+            currentPacket = new PassportScanner.ScannerPacket(data[0], data[1] * 256 + data[2]);
+            usedLen = 3 + currentPacket.addData(data, 3, tmpLen-3);
+        }
+        else
+        {
+            usedLen = currentPacket.addData(data, 0, tmpLen);
+        }
+
+        // There is a rest
+        if(usedLen<tmpLen) {
+            // Reset buffer origin
+            tmpPos = 0;
+            appendBuffer(data, usedLen, tmpLen-usedLen);
+        }
+
+        return currentPacket;
+    }
+
+    @Override
+    protected synchronized void updateReceivedData(byte[] data) {
+        super.updateReceivedData(data);
+        //final String message = "Read " + data.length + " bytes: \n"
+        //        + HexDump.dumpHexString(data) + "\n\n";
+
+        boolean next = true;
+        PassportScanner.ScannerPacket packet = null;
+
+        try {
+            packet = unpackResponse(data);
+            do {
+                if(packet!=null && packet.isComplete()) {
+                    bq.add(packet);
+                    notifyAll();
+                    resetResponse();
+                    packet = unpackResponse(null);
+                } else {
+                    next = false;
+                }
+            }while(next);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private synchronized int send(byte cmd_code, byte [] data, NotificationFlag stopFlag) throws IOException {
+        if (getPort() == null)
+            throw new IOException("No scanner device available");
+
+        // If driver wasn't available during first loading
+        if (!isOpen()) {
+            resume();
+        }
+
+        if (!bq.isEmpty())
+            bq.clear();
+
+        byte[] cmd;
+
+        if (data != null) {
+            cmd = new byte[3 + data.length];
+            cmd[0] = cmd_code;
+            cmd[1] = (byte) (data.length >> 8);
+            cmd[2] = (byte) (data.length & 0xFF);
+            System.arraycopy(data, 0, cmd, 3, data.length);
+        } else {
+            cmd = new byte[]{cmd_code, 0, 0};
+        }
+
+        int written = getPort().write(cmd, 3000);
+
+        // If continuous reading mode is enable expected response is Inquire
+        if (cmd_code == 'C' && data.length > 0 && data[0] == 1)
+            cmd_code = 'I';
+
+        boolean gotResponse = false;
+
+        try {
+            while (bq.isEmpty() && !gotResponse) {
+                if (stopFlag != null && stopFlag.isSet()) {
+                    break;
+                }
+                wait(1000);
+
+                //bq.poll(timeout, TimeUnit.MILLISECONDS);
+
+                while (!bq.isEmpty()) {
+                    if (stopFlag != null && stopFlag.isSet()) {
+                        break;
+                    }
+                    PassportScanner.ScannerPacket packet = bq.poll();
+                    if (cmd_code == packet.getCode())
+                        gotResponse = true;
+
+                    switch (packet.getCode()) {
+                        case 'V':
+                            mScannerVersion = new String(packet.getData(), "ASCII");
+                            break;
+
+                        case 'I':
+                            mMRZ = new String(packet.getData(), "ASCII").split("\r");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            // Ignore
+        }
+        return written;
+    }
+
+
+}
+
 
 
 }
